0ea66ce8436cef86dcba23f07280d6a53e76b3f1
Milenko <kummicko@gmail.com>
2023-03-14 19:29:29 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta praesentium reprehenderit commodi neque labore ad vero eos, possimus repudiandae facilis.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta praesentium reprehenderit commodi neque labore ad vero eos, possimus repudiandae facilis.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 1.25rem;
+    font-size: 2.25rem;
+  }
+  p {
+    margin: 1.25rem 0;
+  }
+</style>
